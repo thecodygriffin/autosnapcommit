59c7f5e7ab82ff20a2a6292a0616d6e507233092
Cody Griffin <thecodygriffin@gmail.com>
2024-02-23 19:24:33 -0500

    Corrected bug in the validate_vm functions, moved vm_state_initial variable, and updated comments.

--- autosnapcommit.sh
+++ autosnapcommit.sh
@@ -7,7 +7,6 @@ set -x
 VM_NAME="${1}"
 VM_DIR="${2}${VM_NAME}/"
 VM_FILE="${VM_DIR}${VM_NAME}.qcow2"
-VM_STATE_INITIAL="$(virsh domstate ${VM_NAME})"
 
 SNAPSHOT_DIR="${VM_DIR}snapshots/"
 SNAPSHOTS_TO_RETAIN=${3}
@@ -19,11 +18,12 @@ LOG_FILE="${VM_NAME}$(date +%Y%m%d%H%M%S).txt"
 function validate_vm() {
   local vm_name_regex="\<${1}\>"
   local existing_vm=( $(virsh list --name --all) )
-  if [[ !  ${existing_vm[@]} =~ ${vm_name_regex} && -f "${VM_FILE}" ]]; then
-    echo "The ${VM_NAME} virtual machine and/or its ${VM_FILE} do not exist."
+  if [[  ${existing_vm[@]} =~ ${vm_name_regex} && -f "${VM_FILE}" ]]; then
+    logger "The ${VM_NAME} virtual machine and its ${VM_FILE} exist."
+  else
+    logger "The ${VM_NAME} virtual machine and/or its ${VM_FILE} do not exist."
     exit 1
   fi
-  echo "The ${VM_NAME} virtual machine and its ${VM_FILE} exist."
 }
 
 # Determines whether or not to abort based command outcome.
@@ -40,7 +40,11 @@ function error_handler() {
 
 # Writes a message to log file.
 function logger() {
-  echo "$(date +%H%M%S) - ${1}" >> "${LOG_DIR}/${LOG_FILE}"
+  if [[ -d "${LOG_DIR}/${LOG_FILE}" ]]; then
+    echo "$(date +%H%M%S) - ${1}" >> "${LOG_DIR}/${LOG_FILE}"
+  else
+    echo "${1}"
+  fi
 }
 
 # Ensures that the directory exists.
@@ -86,8 +90,8 @@ function start_vm() {
   sleep 10 # TODO(codygriffin): Change to 60 after development
 }
 
-# Ensures the virtual machine AppArmor Profile is disabled before performing \
-# a blockcommit.
+# Ensures the virtual machine AppArmor Profile is disabled before creating \
+# a snapshot and performing a blockcommit.
 function disable_apparmor() {
   aa-disable "/etc/apparmor.d/libvirt/libvirt-$(virsh domuuid $VM_NAME)"
   if [[ $? -ne 0 ]]; then
@@ -117,21 +121,32 @@ function create_snapshot() {
   logger "The ${new_snapshot_name} snapshot was created."
 }
 
-
+# Call function to validate that the provided virtual machine parameters are
+# valid.
 validate_vm "${VM_NAME}"
 
+# Call function to validate logs directory exists and creat it if it does not.
 validate_dir "${LOG_DIR}"
 
+# Call function to validate snapshot directory exists and create it if i
+# does not.
 validate_dir "${SNAPSHOT_DIR}"
 
-determine_vm_state 
+# Determine the current state of the vitual machine.
+readonly vm_state_initial="$(virsh domstate ${VM_NAME})"
 
+# Call function to validate that the virtual machine is in an expected state.
+determine_vm_state
+
+# Start the vitual machine if it is not running.
 if [[ "${vm_state_current}" == "shut off" ]]; then
   start_vm
 fi
 
+# Call function to disable the virtual machine AppArmor Profile.
 disable_apparmor
 
+# Call function to create the snapshot.
 create_snapshot
 
 # perform blockcommit
